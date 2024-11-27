1cd707922815fdcf2cc96d2e2f08bb39b43f69b2
pmaroju545 <pavan.maroju@slu.edu>
2024-11-27 05:14:44 +0000

    Week05-lab-submission

---
+++ db.js
@@ -0,0 +1,13 @@
+// db.js
+const mongoose = require('mongoose')
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://root:example@localhost:27017/?authSource=admin',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+module.exports = mongoose
